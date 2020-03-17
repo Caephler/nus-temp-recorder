a6a1013a460ff6c1f8f48e42769c2510a73c0d91
Gabriel <caephler@gmail.com>
2020-03-17 12:25:33 +0800

    Add manual mode support

--- record.py
+++ record.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 
 from dotenv import load_dotenv
 import os
+from sys import argv
 from random import uniform
 
 RECORD_LINK = """https://myaces.nus.edu.sg/htd/htd?loadPage=dlytemperature"""
@@ -15,6 +16,10 @@ def main():
     NUS_PASSWORD = os.getenv("NUS_PASSWORD")
     AM_TEMP = os.getenv("AM_TEMP")
     PM_TEMP = os.getenv("PM_TEMP")
+
+    if argv[1] == '--manual':
+        AM_TEMP = input("Key in your AM temperature:")
+        PM_TEMP = input("Key in your PM temperature:")
 
     if AM_TEMP is None:
         AM_TEMP = generate_random_temp()
@@ -32,44 +37,55 @@ def main():
 def generate_random_temp():
     return "{0:0.1f}".format(uniform(36.1, 36.9))
 
-
 def login(driver, username, password):
     if driver.current_url.split("/")[2] != 'vafs.nus.edu.sg':
         # Check that we are on the login page, if not skip
         return
-    username_elem = driver.find_element_by_id("userNameInput")
-    username_elem.clear()
-    username_elem.send_keys(username)
+    def attempt():
+        username_elem = driver.find_element_by_id("userNameInput")
+        username_elem.clear()
+        username_elem.send_keys(username)
 
-    password_elem = driver.find_element_by_id("passwordInput")
-    password_elem.clear()
-    password_elem.send_keys(password)
+        password_elem = driver.find_element_by_id("passwordInput")
+        password_elem.clear()
+        password_elem.send_keys(password)
 
-    password_elem.send_keys(Keys.RETURN)
+        password_elem.send_keys(Keys.RETURN)
+
+    for i in range(5):
+        try:
+            attempt()
+            return
+        except e:
+            pass
 
 
 
 def submit_reading(driver, temp, part_of_day):
-    # should be on the temperature recording page now
+    def attempt():
+        # should be on the temperature recording page now
+        select_elem = Select(driver.find_element_by_name("declFrequency"))
+        select_elem.select_by_visible_text(part_of_day)
 
-    select_elem = Select(driver.find_element_by_name("declFrequency"))
-    select_elem.select_by_visible_text(part_of_day)
+        temp_elem = driver.find_element_by_id("temperature")
+        temp_elem.send_keys(temp)
 
-    temp_elem = driver.find_element_by_id("temperature")
-    temp_elem.send_keys(temp)
+        symptoms_elem = driver.find_element_by_css_selector("input[type='radio'][value='N']")
+        symptoms_elem.click()
 
-    symptoms_elem = driver.find_element_by_css_selector("input[type='radio'][value='N']")
-    symptoms_elem.click()
+        submit_button = driver.find_element_by_css_selector("input[type='button']")
+        submit_button.click()
 
-    submit_button = driver.find_element_by_css_selector("input[type='button']")
-    submit_button.click()
+        # should be on submit page
+        back_button = driver.find_element_by_css_selector("input[type='button'][value='Back']")
+        back_button.click()
 
-    # should be on submit page
-    back_button = driver.find_element_by_css_selector("input[type='button'][value='Back']")
-    back_button.click()
-
-
-
+    for i in range(5):
+        try:
+            attempt()
+            return
+        except e:
+            pass
 
 
 main()
